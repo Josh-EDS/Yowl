d01d7fe5010ab07768c95b4f7b73ad526fae38a2
josh-EDS <joshua.germany@epitech.digital>
2025-02-10 13:39:25 +0100

    Last Update of Yowl

---
+++ htdocs/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data.text(),
+        icon: 'https://yowl.ct.ws/assets/images/icon.png',
+        badge: 'https://yowl.ct.ws/assets/images/icon.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('New Message', options)
+    );
+});
